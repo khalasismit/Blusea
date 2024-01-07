cfb3acabcce273316355a04b5e96d761522f2a20
Khalasismit <smit67560@gmail.com>
2024-01-07 17:56:05 +0530

    MongoDb Connectivity Changes

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -21,12 +21,12 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
 /* MONGOOSE SETUP */
 
-// const PORT = process.env.PORT || 3002;
-// mongoose.connect(process.env.MONGO_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-//   })
-//   .catch((error) => console.log(`${error} did not connect`));
+const PORT = process.env.PORT || 3002;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT,() => console.log(`Server Port:${PORT} `));
+  })
+  .catch((error) => console.log(` ${error} : server did not connect `));
