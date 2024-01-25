cda98b244a9240727acaa125daea7825490b4087
Khalasismit <smit67560@gmail.com>
2024-01-25 19:09:43 +0530

    init commit, login register commit, redux state commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import AuthPage from './scenes/authPage';
+function App() {
+  return (
+    <div className="app">
+      <AuthPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/scenes/authPage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Form from './Form';
+import { Box } from '@mui/material';
+
+const AuthPage = () => {
+  return <Box>
+    <Form />
+  </Box>
+};
+
+export default AuthPage;
