92a6e407f9d05205ae722be64bfeea122189834b
Khalasismit <smit67560@gmail.com>
2024-02-10 09:22:18 +0530

    explore page ( to display random posts )

--- client/src/scenes/explorePage/index.jsx
+++ client/src/scenes/explorePage/index.jsx
@@ -1,73 +1,49 @@
+import { CircularProgress } from '@mui/material';
 import Box from '@mui/material/Box';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import { useEffect, useState } from 'react';
 
 const ExplorePage = () => {
-    const itemData = [
-        {
-          img: './assets/images/IMG_20231126_173834.jpg',
-          title: 'bed',
-        },
-        {
-          img: './assets/images/IMG_20231126_174200_1.jpg',
-          title: 'Bed',
-        },
-        {
-          img: './assets/images/IMG_20231126_174219.jpg',
-          title: 'Bed',
-        },
-        {
-          img: './assets/images/IMG_20231126_174247.jpg',
-          title: 'Bed',
-        },
-        {
-          img: './assets/images/IMG_20231126_174421.jpg',
-          title: 'Bed',
-        },
-        {
-          img: './assets/images/IMG_20231126_174907.jpg',
-          title: 'Bed',
-        },
-        {
-          img: './assets/images/IMG_20231126_173834.jpg',
-          title: 'bed',
-        },
-        {
-          img: './assets/images/IMG_20231126_174200_1.jpg',
-          title: 'Bed',
-        },
-        {
-          img: './assets/images/IMG_20231126_174219.jpg',
-          title: 'Bed',
-        },
-        {
-          img: './assets/images/IMG_20231126_174247.jpg',
-          title: 'Bed',
-        },
-        {
-          img: './assets/images/IMG_20231126_174421.jpg',
-          title: 'Bed',
-        },
-        {
-          img: './assets/images/IMG_20231126_174907.jpg',
-          title: 'Bed',
-        },
-      ];
-  return (
-    <Box sx={{ width: "100%", height: "100vh",p:"1rem", overflowY: 'scroll' }}>
-      <ImageList variant="masonry" cols={3} gap={4}>
-        {itemData.map((item) => (
-          <ImageListItem key={item.img}>
-            <img
-              srcSet={`${item.img}?w=248&fit=crop&auto=format`}
-              src={`${item.img}?w=248&fit=crop&auto=format`}
-              alt={item.title}
-              loading="lazy"
-            />
-          </ImageListItem>
-        ))}
-      </ImageList>
-    </Box>
-  );
+  const [exploreData, setExploreData] = useState(null);
+  const explore = async () => {
+    let res = await fetch('http://localhost:3001/posts/explore', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await res.json();
+    setExploreData(data);
+  }
+  useEffect(() => {
+    explore()
+  }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  return <Box sx={{ flex: 1, p: "1rem" }}>
+    {
+      Array.isArray(exploreData) ? (
+        <ImageList variant='masonry' cols={3} gap={4}>
+          {
+            exploreData.map((item) => (
+              <ImageListItem key={item._doc._id} >
+                <img
+                  srcSet={`${item.url}`}
+                  src={`${item.url}`}
+                  alt={`${item._doc._id}`}
+                  style={{ objectFit: "cover", borderRadius: "0.5rem" }}
+                />
+              </ImageListItem>
+            ))
+          }
+        </ImageList>
+      ) : (
+
+        <Box sx={{ flex: 1, height: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
+          <CircularProgress />
+        </Box>
+      )
+    }
+  </Box>
 }
 export default ExplorePage;
