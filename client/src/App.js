b212b3f79bfec39f54e350a3db180e7d758f3696
Khalasismit <smit67560@gmail.com>
2024-02-10 09:29:04 +0530

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import AuthPage from './scenes/authPage';
 import HomePage from './scenes/homePage';
 import ExplorePage from './scenes/explorePage';
 import { useSelector } from "react-redux";
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { themeSettings } from './styles/theme/theme';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navigation from './components/nav';
+import ProfilePage from './scenes/profilePage';
+import DialogPost from './components/dialogPost';
+
 function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
@@ -16,31 +18,39 @@ function App() {
   const isNonMobile = useMediaQuery("(min-width:600px)");
   const bgTheme = useTheme();
   const bg = bgTheme.palette.background.alt
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const openDialog = () => {
+    setDialogOpen(true);
+  };
+  const closeDialog = () => {
+    setDialogOpen(false);
+  };
+
   return (
-    <Box sx={{background:{bg}}}>
-        <BrowserRouter>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
+    <Box sx={{ background: { bg } }}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={isAuth ? <Navigate to="/home" /> : <AuthPage />} />
+            {/* <Route path="/" element={<AuthPage />} /> */}
+          </Routes>
+          <Box display={'flex'} flexDirection={isNonMobile ? "row" : "column-reverse"} gap={isNonMobile ? "2rem" : ""}>
+            {isAuth && (
+              <Navigation></Navigation>
+            )}
             <Routes>
-              <Route path="/" element={isAuth ? <Navigate to="/home" /> : <AuthPage />} />
-              {/* <Route path="/" element={<AuthPage />} /> */}
+              <Route exact path="/home" element={isAuth ? <HomePage /> : <Navigate to="/" />} />
+              <Route path="/explore" element={isAuth ? <ExplorePage openDialog={openDialog} /> : <Navigate to="/" />} />
+              <Route path="/profile/:userName" element={isAuth ? <ProfilePage /> : <Navigate to="/" />} />
             </Routes>
-            <Box display={'flex'} flexDirection={isNonMobile ? "row" : "column-reverse"} gap={isNonMobile ? "2rem" : ""}>
-              {isAuth && (
-                <Navigation></Navigation>
-              )
-              }
-              <Routes>
-                <Route path="/home" element={isAuth ? <HomePage /> : <Navigate to="/" />} />
-                <Route path="/explore" element={isAuth ? <ExplorePage /> : <Navigate to="/" />} />
-                {/* <Route
-              path="/profile/:userId"
-              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
-            /> */}
-              </Routes>
-            </Box>
-          </ThemeProvider>
-        </BrowserRouter>
+            <DialogPost dialogOpen={dialogOpen} closeDialog={closeDialog} />
+            {/* <Routes> */}
+              {/* <Route path="/p/:id" element={isAuth ? <DialogPost dialogOpen={dialogOpen} closeDialog={closeDialog} /> : <Navigate to="/" />} /> */}
+            {/* </Routes> */}
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </Box>
   );
 }
