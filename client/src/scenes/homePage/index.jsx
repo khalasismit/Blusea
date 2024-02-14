a4e6996cb9276b8f5fbbe7bf4dfd3fa559908f1d
Khalasismit <smit67560@gmail.com>
2024-02-14 18:39:36 +0530

    homepage changes

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,12 +1,18 @@
 import { Box, useMediaQuery } from "@mui/material";
 import Feed from "../../components/feed";
 import Requests from "../../components/requests";
+import AdSense from "../../components/adsense";
 const HomePage = () => {
     const isNonMobile = useMediaQuery('(min-width:1000px)')
-    return <Box sx={{display:"flex",flex:1,justifyContent:"space-around"}}>
+    return <Box sx={{ display: "flex", flex: 1, justifyContent: "space-around" }}>
         <Feed></Feed>
         {isNonMobile &&
-            (<Requests></Requests>)
+            (
+                <Box sx={{flex:1,display:"flex",flexDirection:"column",justifyContent:"start"}}>
+                    <Requests></Requests>
+                    <AdSense></AdSense>
+                </Box>
+            )
         }
     </Box>
 };
