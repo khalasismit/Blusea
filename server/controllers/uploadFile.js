712283500d53805447f8cb85d0e363a28bc9a6b4
Khalasismit <smit67560@gmail.com>
2024-02-17 16:06:51 +0530

    image compression to Webp to balance the load ( before upload )

--- server/controllers/uploadFile.js
+++ server/controllers/uploadFile.js
@@ -1,5 +1,6 @@
 import express from "express";
 import multer from "multer";
+import sharp from "sharp";
 import dotenv from "dotenv";
 dotenv.config();
 import admin from "firebase-admin";
@@ -19,19 +20,24 @@ router.post('/upload', upload.single("file"), async (req, res) => {
   try {
     // console.log(req);
     const file = req.file
+    const compressedImageBuffer = await sharp(file.buffer)
+    .rotate()
+    .resize({ width: 800 }) // Adjust dimensions as needed
+    .webp()
+    .toBuffer();
     // console.log(file);
-    const { originalname, buffer, mimetype } = file;
+    const { originalname, mimetype } = file;
     const bucket = admin.storage().bucket();
-    const fileName = `${Date.now()}-${originalname}`;
+    const fileName = `${Date.now()}-${originalname}.webp`;
     const fileUpload = bucket.file(fileName);
 
     const stream = fileUpload.createWriteStream({
       metadata: {
-        contentType: mimetype,
+        contentType: 'image/webp',
       },
     });
 
-    stream.end(buffer);
+    stream.end(compressedImageBuffer);
     // const downloadUrl = `https://storage.googleapis.com/${bucket.name}/${fileName}`;
     const [downloadUrl] = await fileUpload.getSignedUrl({
       action: 'read',
@@ -42,7 +48,7 @@ router.post('/upload', upload.single("file"), async (req, res) => {
     const newFile = new File({
       name: fileName,
       originalName: originalname,
-      mimetype: mimetype,
+      mimetype: 'image/webp',
       url: downloadUrl,
     });
     await newFile.save();
@@ -54,6 +60,23 @@ router.post('/upload', upload.single("file"), async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.post('/delete', async (req, res) => {
+  try {
+    const { fileId } = req.body;
+    if (!mongoose.Types.ObjectId.isValid(fileId)) throw new Error('Invalid File ID');
+
+    const result = await File.findByIdAndDelete(fileId);
+    if (!result) throw new Error("Couldn't find the file");
+
+    gcs.bucket(config.GCS_BUCKET).file(`images/${result.name}`).delete();
+
+    res.sendStatus(204);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({ error: err.toString() });
   }
 });
 
