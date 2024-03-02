c12a54b2c35010f5e2c2450fb248d649912e0359
Khalasismit <smit67560@gmail.com>
2024-03-02 22:05:37 +0530

    socket io authentication modification

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,13 +1,29 @@
 import { Box, useMediaQuery } from "@mui/material";
 import Feed from "../../components/feed";
 import Requests from "../../components/requests";
+import Ads from "../../components/ads";
+import { useEffect } from "react";
+import { io } from "socket.io-client";
+import { useSelector } from "react-redux";
 const HomePage = () => {
+    const socket = io("http://localhost:3001");
+    const user = useSelector((state) => state.user)
     const isNonMobile = useMediaQuery('(min-width:1000px)')
-    return <Box sx={{ display: "flex", flex: 1, justifyContent: "space-around" }}>
+    useEffect(()=>{
+        socket.on("connect",()=>{
+            socket.emit("authenticate", user._id);
+        })
+    },[])
+    
+    return <Box sx={{ display: "flex", flex: 1, justifyContent: "space-evenly" }}>
+        {/* commented // */}
         <Feed></Feed>
         {isNonMobile &&
             (
-                <Requests></Requests>
+                <Box sx={{ height: "100vh", flex: 1, gap: 1, display: "flex", flexDirection: "column" }}>
+                        <Requests></Requests>
+                        <Ads></Ads>
+                </Box>
             )
         }
     </Box>
